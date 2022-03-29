b6aed2b105ecd4dcac90b58d062193b308821573
Ruben Rodrigues <ruben2000@gmail.com>
2022-03-29 14:32:45 +0100

    Day 3 fully done, part 2 included

--- 2015/day3.py
+++ 2015/day3.py
@@ -25,23 +25,44 @@ def part1():
 
 
 def part2():
-    inputValues = common.getInputOfDay(2)
-    totalRibbon = 0
-    for box in inputValues:
-        measures = box.split('x')
-        length = int(measures[0])
-        width = int(measures[1])
-        height = int(measures[2])
-        measuresAsInts = [length, width, height]
-        measuresAsInts.sort()
-        bow = length * width * height
-        ribbonToWrap = measuresAsInts[0] * 2 + measuresAsInts[1] * 2
-        totalRibbon += ribbonToWrap + bow
-    return totalRibbon
+    inputValues = common.getInputOfDay(3)
+    directions = inputValues[0]
+    indexXRealSanta = 0
+    indexYRealSanta = 0
+    indexXRobotSanta = 0
+    indexYRobotSanta = 0
+    indexX = 0
+    indexY = 0
+    visitedSpots = set()
+    moveCounter = 1
+    for move in directions:
+        if moveCounter % 2 == 0:
+            indexX = indexXRobotSanta
+            indexY = indexYRobotSanta
+        else:
+            indexX = indexXRealSanta
+            indexY = indexYRealSanta
+        visitedSpots.add((indexX, indexY))
+        if move == '>':
+            indexX += 1
+        elif move == '<':
+            indexX -= 1
+        elif move == '^':
+            indexY += 1
+        elif move == 'v':
+            indexY -= 1
+        if moveCounter % 2 == 0:
+            indexXRobotSanta = indexX
+            indexYRobotSanta = indexY
+        else:
+            indexXRealSanta = indexX
+            indexYRealSanta = indexY
+        moveCounter += 1
+    return len(visitedSpots)
 
 
 def main():
-    print(part1())
+    print(part2())
 
 
 if __name__ == "__main__":
