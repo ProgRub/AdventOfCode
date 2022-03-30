000add2a593b083a891350b8a6de37ca99c51ace
Ruben Rodrigues <ruben2000@gmail.com>
2022-03-30 17:08:16 +0100

    Input files and python files for every day, every year so far; generate files automatic file generation given one year or two years, files for every year in range

--- generateFiles.py
+++ generateFiles.py
@@ -1,13 +1,33 @@
-# file to create input files and python files for each day
+import sys
+from os.path import exists
+from pathlib import Path
 
-file = open("example.py", "w")
-file.write("import common\n\n\n")
-file.write("def part1():\n    inputValues = common.getInputOfDay()\n    return inputValues\n\n\n")
-file.write("def part2():\n    inputValues = common.getInputOfDay()\n    return inputValues\n\n\n")
-file.write("def main():\n    print(part1())\n\n\n")
-file.write("if __name__ == \"__main__\":\n    main()")
 
-for year in range(2015, 2022):
-    for day in range(1, 26):
-        print(f"{year}\\inputs\\day{day}.txt")
-        print(f"{year}\\day{day}.py")
+def createFiles(startYear, endYear):
+    for year in range(startYear, endYear):
+        if not exists(str(year)):
+            Path(f"{year}\\inputs").mkdir(parents=True, exist_ok=True)
+        for day in range(1, 26):
+            textFilename = f"{year}\\inputs\\day{day}.txt"
+            pythonFilename = f"{year}\\day{day}.py"
+            if exists(textFilename) and exists(pythonFilename):
+                continue
+            textFile = open(textFilename, "w")
+            textFile.write("Input here.")
+            textFile.close()
+
+            pythonFile = open(pythonFilename, "w")
+            pythonFile.write("import common\n\n\n")
+            pythonFile.write("def part1():\n    inputValues = common.getInput()\n    return inputValues\n\n\n")
+            pythonFile.write("def part2():\n    inputValues = common.getInput()\n    return inputValues\n\n\n")
+            pythonFile.write("def main():\n    print(part1())\n\n\n")
+            pythonFile.write("if __name__ == \"__main__\":\n    main()")
+            pythonFile.close()
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    if len(arguments) == 1:
+        createFiles(int(arguments[0]), int(arguments[0]) + 1)
+    else:
+        createFiles(int(arguments[0]), int(arguments[1])+1)
