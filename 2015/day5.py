9fa3c6c0d77e7f0f3205d673db4adef708117916
Ruben Rodrigues <ruben2000@gmail.com>
2022-03-30 22:55:27 +0100

    Day 5 part 1 done

--- 2015/day5.py
+++ 2015/day5.py
@@ -3,7 +3,21 @@ import common
 
 def part1():
     inputValues = common.getInput()
-    return inputValues
+    niceStrings = 0
+    for string in inputValues:
+        amountOfVowels = 0
+        twoLettersInARow = False
+        if "ab" in string or "cd" in string or "pq" in string or "xy" in string:
+            continue
+        for index in range(len(string)):
+            if string[index] == 'a' or string[index] == 'e' or string[index] == 'i' or string[index] == 'o' or string[index] == 'u':
+                amountOfVowels += 1
+            if index < len(string) - 1 and string[index + 1] == string[index]:
+                twoLettersInARow = True
+        if amountOfVowels >= 3 and twoLettersInARow:
+            niceStrings += 1
+
+    return niceStrings
 
 
 def part2():
