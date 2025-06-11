a245736f7ca76ddcf7c77861fcfd826808eebf23
ProgRub <ruben2000jgr@gmail.com>
2025-06-11 08:49:24 +0100

    2015, day 8, part 2 done

--- 2015/day8.py
+++ 2015/day8.py
@@ -20,14 +20,34 @@ def part1():
         stringsLength += len(line)
     return totalLength - stringsLength
 
+def encode(line: str) -> str:
+    encodedLine = ''
+    aux = line[1:len(line) - 1]
+    for char in line[1:len(line) - 1]:
+        match char:
+            case "\\":
+                encodedLine += "\\" + char
+            case "\"":
+                encodedLine += "\\\""
+            case _:
+                encodedLine += char
+    return f"\"\\\"{encodedLine}\\\"\""
+
 
 def part2():
-    inputValues = common.getInput()
-    return inputValues
-
+    inputValues = common.getInput()    
+    totalLength = 0
+    stringsLength = 0
+    for line in inputValues:
+        totalLength += len(line)
+        encodedLine = encode(line)
+        print(f"{line}: {len(line)}")
+        print(f"{encodedLine}: {len(encodedLine)}")
+        stringsLength += len(encodedLine) 
+    return stringsLength - totalLength 
 
 def main():
-    print(part1())
+    print(part2())
 
 
 if __name__ == "__main__":
