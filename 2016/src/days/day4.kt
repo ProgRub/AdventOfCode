0d1c25dc44db41712b028a7bb1d5ae62474e1b02
ProgRub <ruben2000jgr@gmail.com>
2026-08-18 11:01:51 +0100

    Day 4 done

--- 2016/src/days/day4.kt
+++ 2016/src/days/day4.kt
@@ -1,26 +1,104 @@
 package days
 
-class Day4: IDaySolver{
+class Day4 : IDaySolver {
     override var parser: IParser = Day4Parser()
     override var problemInput: List<String>
+    private var roomCodes: List<RoomCode>
 
     constructor(input: List<String>) {
-        this.problemInput = this.parser.parseTextToList(input[0],",")
+        this.problemInput = input
+        this.roomCodes = this.problemInput.map { RoomCode(it) }
     }
 
     override fun test(): String {
-        TODO("Not yet implemented")
+        val sum = roomCodes.sumOf { roomCode -> if (roomCode.isReal()) roomCode.id else 0 }
+        return sum.toString()
     }
 
     override fun part1(): String {
-        TODO("Not yet implemented")
+        val sum = roomCodes.sumOf { roomCode -> if (roomCode.isReal()) roomCode.id else 0 }
+        return sum.toString()
     }
 
+    // We have to find the room ID where north pole objects are stored, that's why we filter the real names for "north"
     override fun part2(): String {
-        TODO("Not yet implemented")
+        val realRooms = roomCodes.filter { roomCode -> roomCode.isReal() }
+        val northPoleStorage = realRooms.first { it.findRealName().contains("north") }
+        return northPoleStorage.id.toString()
     }
 }
 
-class Day4Parser : IParser{
+internal class RoomCode {
+    var checkSum: String
+    var id: Int = -1
+    var name: String
+    var nameSplit: List<String>
 
+    constructor(line: String) {
+        val splitIterator = line.splitToSequence("-")
+        name = ""
+        checkSum = ""
+        nameSplit = listOf()
+        for (value in splitIterator) {
+            // If it's the last value of the iterator it's the id and the checksum; ex.: 567[abcde]
+            if (value == splitIterator.last()) {
+                id = value.substring(0, 3).toInt()
+                checkSum = value.substring(4, 9)
+            }
+            // Otherwise add to the name, and the split list of the name
+            else {
+                name += value
+                nameSplit += value
+            }
+        }
+    }
+
+    // Determines if the encrypted name of the room  points to a real room
+    // It's real if the characters in the checksum are the most common characters in the name
+    // Ordered in the checksum from most common to least common, in the case of a tie needs to be ordered alphabetically
+    internal fun isReal(): Boolean {
+        val lettersCount = sortedMapOf<Char, Int>()
+        this.name.toCharArray().forEach { letter -> lettersCount[letter] = this.name.count { it == letter } }
+        var previousHigh = lettersCount.values.maxOrNull() ?: 0
+        var previousLetter = this.checkSum[0]
+        // If the first letter of the checksum doesn't appear in the word
+        // or the count of the first letter doesn't equal the biggest value then checksum isn't ordered
+        if (previousLetter !in lettersCount) return false
+        if (lettersCount[previousLetter]!! != previousHigh) return false
+        // Start from the second letter
+        for (letter in this.checkSum.substring(1)) {
+            // If the checksum letter doesn't exist in the name, can't be in the checksum
+            if (letter !in lettersCount) return false
+            val count = lettersCount[letter]!!
+            // If the count of this letter is greater than the count of the previous one then checksum isn't ordered
+            if (count > previousHigh) return false
+            // If the count of this letter is equal to the previous one and
+            // this letter is "smaller" than the previous one then the checksum isn't ordered alphabetically in a tie
+            if (count == previousHigh && letter < previousLetter) return false
+            previousHigh = count
+            previousLetter = letter
+        }
+        return true
+    }
+
+    // Decrypts the name
+    internal fun findRealName(): String {
+        var realName = ""
+        val lastLetterValue = 'z'.code
+        val firstLetterCode = 'a'.code -1 // Remove one so we consider a
+        val divider = lastLetterValue - firstLetterCode // number of letters in alphabet
+        val shiftForward = this.id % divider
+        for (encrypWord in this.nameSplit) {
+            for (letter in encrypWord) {
+                var code = letter.code + shiftForward
+                // If we've gone past z need to come back around
+                if (code > lastLetterValue) code = code - lastLetterValue + firstLetterCode
+                realName += Char(code).toString()
+            }
+            realName += " "
+        }
+        return realName
+    }
 }
+
+class Day4Parser : IParser
---
+++ 2016/src/unitTests/Day4Test.kt
@@ -0,0 +1,14 @@
+import days.Day4
+import org.junit.jupiter.api.BeforeEach
+
+class Day4Test : DayTester(){
+
+    @BeforeEach
+    override fun setUp() {
+        testDay = 4
+        part1Solution = "361724"
+        part2Solution = "482"
+        super.setUp()
+        solver = Day4(inputText)
+    }
+}
